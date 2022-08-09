c0eb80001885a81519fb77068bf1a45345dba5ee
Duarte Ribeiro <duarib@gmail.com>
2022-08-09 17:20:19 +0100

    Removing Docusaurus Files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\nuno\\Documents\\GitHub\\assets\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\nuno\\Documents\\GitHub\\assets\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\nuno\\Documents\\GitHub\\assets\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\nuno\\Documents\\GitHub\\assets\\src\\css\\custom.css'),
-];
