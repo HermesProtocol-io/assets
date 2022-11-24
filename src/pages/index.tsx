7ea6ad41078fd5e94beb85944d799e425c6e8a0d
Nuno Morais <nunodpmorais@gmail.com>
2022-11-24 11:53:30 +0000

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -17,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://github.com/HermesProtocol-io/assets">
+            📁 See this Repo on Github
           </Link>
         </div>
       </div>
@@ -30,8 +29,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description={`${siteConfig.title}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
